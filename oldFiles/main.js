237d9a9ceaae047308a43af509e57617d3e64486
YosiLeibman <yosil.770@gmail.com>
2019-03-07 16:47:35 +0200

    clean ups, coments and logics

--- oldFiles/main.js
+++ oldFiles/main.js
@@ -4,15 +4,12 @@ let moves = 0;
 let currCell;
 let currPlayer;
 let isWinner = false;
-
-// event on click on each btn
 let clickee=(id)=> {
-    //get the current elmelnt
     currCell=document.getElementById(id);
     
     // if there are still empty cells to play? 
     if(moves <=9){
-        // if so, this spesific place is empty?
+        console.log(currCell.textContent);
         if(currCell.textContent=='#'){
             play();
         }else{
@@ -24,9 +21,7 @@ let clickee=(id)=> {
     
 }
 
-// main function, the game
 let play = () => {
-    // first of all, move's counter
     moves++;
     
     // determinate the sign on the bord
@@ -36,44 +31,59 @@ let play = () => {
         currPlayer='O';
     }
 
-    // mark the correct sign to the board
-    currCell.textContent = currPlayer;
-
-    // after every move - need check if there's a winner
-    checkWin();
+     currCell.textContent = currPlayer;
+     checkWin();
+  
     
-    // if there isn't - let the computer make his turn
+    // let myPromis = new Promise((resolve, reject)=>{
+    //     currCell.textContent = currPlayer;
+    //     resolve(currCell);
+    //     reject(currCell);
+    // })
+    // myPromis.then(()=>{
+    //     checkWin();
+    // }).then(
+    //     ()=>{
+    //         if (!isWinner) {
+    //             // get comp random choice
+    //             compPlay();
+    //             checkWin();
+    //         }
+    //     }
+    // )
+
+    // checkWin();
+    // it 9 moves only' i cannot call this on
+    // // the last turn, its only users
     if (!isWinner) {
         // get comp random choice
         compPlay();
-        // after every move - need check if there's a winner
         checkWin();
     }
-
-   
+    console.log(currPlayer);
+    // they all start with # so it equals
+    // if (moves > 4) {
+    //     checkWin();
+    // }
 }
 
-// the computer turm generator
 let compPlay = () =>{
-    // inc the move
-    moves++;  
-    // flag loop  
+    moves++;    
     let isNotEmpty = true;
     while(isNotEmpty){
-        // generate some random between 1-9
-        let x =Math.floor(Math.random()*9) + 1;
-        // store the element
-        let checkedCell=document.getElementById(x.toString());
-        // check for emptyness
-        if(checkedCell.textContent == '#'){
-            isNotEmpty = false;
-            checkedCell.textContent = 'O';
+    let x =Math.floor(Math.random()*9) + 1;
+
+    let checkedCell=document.getElementById(x.toString());
+    // console.log(checkedCell)
+    if(checkedCell.textContent == '#'){
+         isNotEmpty = false;
+         checkedCell.textContent = 'O';
         }
     }
 }
 
-// check the board for winning situation
 let checkWin= () => {
+    // better option is to extact them t an array and then work with this array only
     if((document.getElementById('1').textContent==document.getElementById('2').textContent &&
     document.getElementById('2').textContent==document.getElementById('3').textContent && document.getElementById('2').textContent!='#') || 
     (document.getElementById('4').textContent==document.getElementById('5').textContent &&
@@ -90,37 +100,27 @@ let checkWin= () => {
     document.getElementById('5').textContent==document.getElementById('8').textContent && document.getElementById('5').textContent!='#')||
     (document.getElementById('3').textContent==document.getElementById('6').textContent &&
     document.getElementById('6').textContent==document.getElementById('9').textContent && document.getElementById('6').textContent!='#')   ){
-        // change the boolean to stop computer from make the next move
         isWinner = true;
         // stop the game when there is a winner
         for(let i = 1; i <= 9; i++){
-            // by removing the event listener
             document.getElementById(i.toString()).removeAttribute("onclick");
         }
-        // alerting the user for win
+        // alert('there is a winner');
+        console.log('there is a winner');
         setTimeout(() => {
             alert('there is a winner');
         }, 200);
     }
 }
 
-// restart the game - called from event on restart btn
+
 const restart = () =>{
-    // run over all game fileds
     for(let i = 1; i <= 9; i++){
-        // get the element
-        let x = document.getElementById(i.toString());
-        // reasign init. value
-        x.textContent = '#';
-        // reasign the click event
-        x.addEventListener('click',(e)=>{
-            clickee(x.id);
-        });
+        document.getElementById(i.toString()).textContent='#';
+        // document.getElementById(i.toString()).addEventListener("click", clickee(i));
+
     }
-    // restart the move's counter
-    moves = 0;
-    // restart the winner boolean
-    isWinner = false;
+    moves=0;
 }
 
 
